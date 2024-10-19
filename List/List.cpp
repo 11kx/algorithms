5606c1258e066d3ce1355c82aed9d79854553166
孔鑫 <1578818828@qq.com>
2024-10-19 08:03:45 +0800

    单链表添加-合并两个有序单链表方法

--- List/List.cpp
+++ List/List.cpp
@@ -62,7 +62,7 @@ public:
 void inverseList(Node *head)
 {
     Node *p = head->next;
-    if(p == nullptr)
+    if (p == nullptr)
     {
         return;
     }
@@ -75,17 +75,58 @@ void inverseList(Node *head)
         p = q;
     }
 }
+// 合并两个有序单链表，输出合并后的有序单链表。有序链表直接在List1上进行合并
+void mergeTwoLists(LinkedList &list1, LinkedList &list2)
+{
+    Node *p = list1.head->next;
+    Node *q = list2.head->next;
+    // 指向合并后新链表的最后一个元素，
+    //起始位置应该位于List1的头结点位置，而不是list.head->next
+    //因为你也不能确定list.head->next的数字是否符合新链表的起始位置。
+    Node *last = list1.head;
+    list2.head->next = nullptr;
+    while (p->next != nullptr && q->next != nullptr)
+    {
+        if(p->data < q->data)
+        {
+            last->next = p;
+            p = p->next;
+        }
+        else
+        {
+            last->next = q;
+            q = q->next;
+        }
+        last = last->next;
+    }
+    if(p != nullptr)
+    {
+        last->next = p;
+    }
+    if(q != nullptr)
+    {
+        last->next = q;
+    }
+}
 
 int main()
 {
-    LinkedList list;
-    list.insertAtTail(15);
-    list.insertAtTail(27);
-    list.insertAtTail(32);
-    list.insertAtTail(45);
+    int arr[] = {1, 2, 4, 6, 7};
+    int brr[] = {1, 3, 4, 9};
+    LinkedList list1;
+    LinkedList list2;
+    for (auto i : arr)
+    {
+        list1.insertAtTail(i);
+    }
+    for (auto i : brr)
+    {
+        list2.insertAtTail(i);
+    }
 
-    list.printList();
-    inverseList(list.head);
+    list1.printList();
+    list2.printList();
+    mergeTwoLists(list1, list2);
 
-    list.printList();
+    list1.printList();
 }
